e06f45dc9d4c31981210f4ae84603235083e76d6
leandro <leandrors91@gmail.com>
2023-12-28 19:31:34 -0300

    implementa propriedades, herda classe abstrata, realiza teste

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("11-99896-8016","c12","111111-11-111111-1",64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsaap");
+
+Smartphone iphone = new Iphone("11-99896-8016","c12","111111-11-111111-1",64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("whatsaap");
